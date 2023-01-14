6e4350caa423e361623fbf83403e9ce6d28955fb
wunuit <112681890+wunuit@users.noreply.github.com>
2023-01-14 12:06:32 +0800

    Add files via upload

--- restart.sh
+++ restart.sh
@@ -1,219 +1,1 @@
-#!/bin/bash
-# Menu For Script
-# Edition : Stable Edition V1.0
-# Auther  : 
-# (C) Copyright 2021-2022
-# =========================================
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-UWhite='\033[4;37m'       # White
-On_IPurple='\033[0;105m'  #
-On_IRed='\033[0;101m'
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-NC='\e[0m'
-
-# // Export Color & Information
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
-
-# // Export Banner Status Information
-export EROR="[${RED} EROR ${NC}]"
-export INFO="[${YELLOW} INFO ${NC}]"
-export OKEY="[${GREEN} OKEY ${NC}]"
-export PENDING="[${YELLOW} PENDING ${NC}]"
-export SEND="[${YELLOW} SEND ${NC}]"
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
-
-# // Export Align
-export BOLD="\e[1m"
-export WARNING="${RED}\e[5m"
-export UNDERLINE="\e[4m"
-
-# // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/wunuit/test/main"
-export Server1_URL="raw.githubusercontent.com/wunuit/limit/main"
-export Server_Port="443"
-export Server_IP="underfined"
-export Script_Mode="Stable"
-export Auther=".geovpn"
-
-# // Root Checking
-if [ "${EUID}" -ne 0 ]; then
-		echo -e "${EROR} Please Run This Script As Root User !"
-		exit 1
-fi
-
-# // Exporting IP Address
-export IP=$( curl -s https://ipinfo.io/ip/ )
-
-# // Exporting Network Interface
-export NETWORK_IFACE="$(ip route show to default | awk '{print $5}')"
-
-# // Validate Result ( 1 )
-touch /etc/${Auther}/license.key
-export Your_License_Key="$( cat /etc/${Auther}/license.key | awk '{print $1}' )"
-export Validated_Your_License_Key_With_Server="$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 1 )"
-if [[ "$Validated_Your_License_Key_With_Server" == "$Your_License_Key" ]]; then
-    validated='true'
-else
-    echo -e "${EROR} License Key Not Valid"
-    exit 1
-fi
-
-# // Checking VPS Status > Got Banned / No
-if [[ $IP == "$( curl -s https://${Server_URL}/blacklist.txt | cut -d ' ' -f 1 | grep -w $IP | head -n1 )" ]]; then
-    echo -e "${EROR} 403 Forbidden ( Your VPS Has Been Banned )"
-    exit  1
-fi
-
-# // Checking VPS Status > Got Banned / No
-if [[ $Your_License_Key == "$( curl -s https://${Server_URL} | cut -d ' ' -f 1 | grep -w $Your_License_Key | head -n1)" ]]; then
-    echo -e "${EROR} 403 Forbidden ( Your License Has Been Limited )"
-    exit  1
-fi
-
-# // Checking VPS Status > Got Banned / No
-if [[ 'Standart' == "$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 6 )" ]]; then 
-    License_Mode='Standart'
-elif [[ Pro == "$( curl -s https://${Server_URL}/validated-registered-license-key.txt | grep -w $Your_License_Key | head -n1 | cut -d ' ' -f 6 )" ]]; then 
-    License_Mode='Pro'
-else
-    echo -e "${EROR} Please Using Genuine License !"
-    exit 1
-fi
-
-# // Checking Script Expired
-exp=$( curl -s https://${Server1_URL}/limit.txt | grep -w $IP | cut -d ' ' -f 3 )
-now=`date -d "0 days" +"%Y-%m-%d"`
-expired_date=$(date -d "$exp" +%s)
-now_date=$(date -d "$now" +%s)
-sisa_hari=$(( ($expired_date - $now_date) / 86400 ))
-if [[ $sisa_hari -lt 0 ]]; then
-    echo $sisa_hari > /etc/${Auther}/license-remaining-active-days.db
-    echo -e "${EROR} Your License Key Expired ( $sisa_hari Days )"
-    exit 1
-else
-    echo $sisa_hari > /etc/${Auther}/license-remaining-active-days.db
-fi
-
-clear
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e ""
-echo -e "    [1] Restart All Services"
-echo -e "    [2] Restart OpenSSH"
-echo -e "    [3] Restart Dropbear"
-echo -e "    [4] Restart Stunnel5"
-echo -e "    [5] Restart Nginx"
-echo -e "    [6] Restart Badvpn"
-echo -e "    [x] Menu"
-echo -e ""
-read -p "    Select From Options [1-6 or x] :  " Restart
-echo -e ""
-echo -e "======================================"
-sleep 1
-clear
-case $Restart in
-                1)
-                clear
-                #systemctl restart ws-dropbear.service >/dev/null 2>&1
-                systemctl restart ws-stunnel.service >/dev/null 2>&1
-                systemctl restart xray.service >/dev/null 2>&1
-                /etc/init.d/ssh restart
-                /etc/init.d/dropbear restart
-                /etc/init.d/stunnel5 restart
-                /etc/init.d/fail2ban restart
-                /etc/init.d/cron restart
-                /etc/init.d/nginx restart
-                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
-                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
-                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
-                systemctl restart rc-local.service
-                echo -e ""
-                echo -e "======================================"
-                echo -e ""
-                echo -e "          Service/s Restarted         "
-                echo -e ""
-                echo -e "======================================"
-                ;;
-                2)
-                clear
-                /etc/init.d/ssh restart
-                echo -e ""
-                echo -e "======================================"
-                echo -e ""
-                echo -e "         SSH Service Restarted        "
-                echo -e ""
-                echo -e "======================================"
-                ;;
-                3)
-                clear
-                /etc/init.d/dropbear restart
-                echo -e ""
-                echo -e "======================================"
-                echo -e ""
-                echo -e "       Dropbear Service Restarted     "
-                echo -e ""
-                echo -e "======================================"
-                ;;
-                4)
-                clear
-                /etc/init.d/stunnel5 restart
-                echo -e ""
-                echo -e "======================================"
-                echo -e ""
-                echo -e "        Stunnel5 Service Restarted    "
-                echo -e ""
-                echo -e "======================================"
-                ;;
-                5)
-                clear
-                /etc/init.d/nginx restart
-                echo -e ""
-                echo -e "======================================"
-                echo -e ""
-                echo -e "         Nginx Service Restarted      "
-                echo -e ""
-                echo -e "======================================"
-                ;;
-                6)
-                clear
-                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
-                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
-                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
-                systemctl restart rc-local.service >/dev/null 2>&1
-                echo -e ""
-                echo -e "======================================"
-                echo -e ""
-                echo -e "    Badvpn  Badvpn Service Restarted  "
-                echo -e ""
-                echo -e "======================================"
-                ;;
-                x)
-                clear
-                menu
-                ;;
-        esac
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
+#!/bin/bash# Menu For Script# Edition : Stable Edition V1.0# Auther  : # (C) Copyright 2021-2022# =========================================clearfun_bar() {    CMD[0]="$1"    CMD[1]="$2"    (        [[ -e $HOME/fim ]] && rm $HOME/fim        ${CMD[0]} -y >/dev/null 2>&1        ${CMD[1]} -y >/dev/null 2>&1        touch $HOME/fim    ) >/dev/null 2>&1 &    tput civis    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["    while true; do        for ((i = 0; i < 18; i++)); do            echo -ne "\033[0;32m▆"            sleep 0.1s        done        [[ -e $HOME/fim ]] && rm $HOME/fim && break        echo -e "\033[0;33m]"        sleep 1s        tput cuu1        tput dl1        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["    done    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"    tput cnorm}res1() {    systemctl daemon-reload}res2() {    systemctl restart nginx}res3() {    systemctl restart xray}res4() {    systemctl restart rc-local}res5() {    systemctl restart client}res6() {    systemctl restart ssh}res7() {    systemctl restart dropbear}res8() {    systemctl restart ws-stunnel}res9() {    systemctl restart openvpn}res10() {    systemctl restart cron}res11() {    systemctl restart haproxy}res12() {    systemctl restart netfilter-persistent}res13() {    systemctl restart squid}res14() {    systemctl restart badvpn1    systemctl restart badvpn2    systemctl restart badvpn3}netfilter-persistentclearecho -e "\033[0;33m ┌──────────────────────────────────────────┐\033[0m"echo -e "\033[0;33m │\033[0m          \033[0;32mRESTART SERVICE SERVER\033[0m          \033[0;33m|\033[0m"echo -e "\033[0;33m └──────────────────────────────────────────┘\033[0m"echo -e ""echo -e "  \033[1;91m service daemon-reload\033[1;37m"fun_bar 'res1'echo -e "  \033[1;91m service restart nginx\033[1;37m"fun_bar 'res2'echo -e "  \033[1;91m service restart xray\033[1;37m"fun_bar 'res3'echo -e "  \033[1;91m service restart rc-local\033[1;37m"fun_bar 'res4'echo -e "  \033[1;91m service restart ssh\033[1;37m"fun_bar 'res6'echo -e "  \033[1;91m service restart dropbear\033[1;37m"fun_bar 'res7'echo -e "  \033[1;91m service restart websocket\033[1;37m"fun_bar 'res8'echo -e "  \033[1;91m service restart cron\033[1;37m"fun_bar 'res10'echo -e "  \033[1;91m service restart netfilter-persistent\033[1;37m"fun_bar 'res12'echo -e ""read -n 1 -s -r -p "Press any key to back on menu"menu
