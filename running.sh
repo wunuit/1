cf5f2ef6f10218ae7a1134f4d4540713b4ec4add
wunuit <112681890+wunuit@users.noreply.github.com>
2023-01-14 14:22:43 +0800

    Add files via upload

--- running.sh
+++ running.sh
@@ -119,6 +119,7 @@ Iptables=$(systemctl status netfilter-persistent | grep active | awk '{print $3}
 #BadVPN=$(systemctl status badvpn3 | grep active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g')
 RClocal=$(systemctl status rc-local | grep active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g')
 Autorebot=$(systemctl status rc-local | grep active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g')
+Stunnel=$(systemctl status stunnel5 | grep active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g')
 if [[ $Open_SSH == "running" ]]; then
     status_1="${grenbo}Online${NC}"
 else
@@ -131,143 +132,102 @@ else
     status_2="${RED}Offline${NC}"
 fi
 
-#if [[ $Dropbear_Websocket == "running" ]]; then
-    status_3="${grenbo}Online${NC}"
-#else
-  #  status_3="${RED}Offline${NC}"
-#fi
-
 if [[ $SSH_Websocket == "running" ]]; then
+    status_3="${grenbo}Online${NC}"
+else
+    status_3="${RED}Offline${NC}"
+fi
+
+if [[ $Nginx_Webserver == "running" ]]; then
     status_4="${grenbo}Online${NC}"
 else
     status_4="${RED}Offline${NC}"
 fi
 
-#if [[ $OpenVPN == "exited" ]]; then
- #   status_5="${grenbo}Online${NC}"
-#else
+if [[ $XRAY_Vmess_TLS == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
     status_5="${RED}Offline${NC}"
-#fi
+fi
 
-if [[ $Nginx_Webserver == "running" ]]; then
+if [[ $XRAY_Vmess_gRPC == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $XRAY_Vmess_None_TLS == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $XRAY_Vless_TLS == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $XRAY_Vless_gRPC == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $XRAY_Vless_None_TLS == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $Trojan_gRPC == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $Trojan_WS == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $Shadowsocks_WS == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $Shadowsocks_gRPC == "running" ]]; then
+    status_5="${grenbo}Online${NC}"
+else
+    status_5="${RED}Offline${NC}"
+fi
+
+if [[ $Iptables == "exited" ]]; then
     status_6="${grenbo}Online${NC}"
 else
     status_6="${RED}Offline${NC}"
 fi
 
-#if [[ $Haproxy == "running" ]]; then
-  #  status_7="${grenbo}Online${NC}"
-#else
-  #  status_7="${RED}Offline${NC}"
-#fi
-
-#if [[ $DNS_Server == "running" ]]; then
-   # status_8="${grenbo}Online${NC}"
-#else
-  #  status_8="${RED}Offline${NC}"
-#fi
-
-#if [[ $DNS_Client == "running" ]]; then
-  #  status_9="${grenbo}Online${NC}"
-#else
-  #  status_9="${RED}Offline${NC}"
-#fi
-
-#if [[ $OpenVPN_Websocket == "running" ]]; then
-   # status_10="${grenbo}Online${NC}"
-#else
-  #  status_10="${RED}Offline${NC}"
-#fi
-
-if [[ $XRAY_Vmess_TLS == "running" ]]; then
-    status_11="${grenbo}Online${NC}"
+if [[ $RClocal == "running" ]]; then
+    status_7="${grenbo}Online${NC}"
 else
-    status_11="${RED}Offline${NC}"
+    status_7="${RED}Offline${NC}"
 fi
 
-if [[ $XRAY_Vmess_gRPC == "running" ]]; then
-    status_12="${grenbo}Online${NC}"
+if [[ $Autorebot == "running" ]]; then
+    status_7="${grenbo}Online${NC}"
 else
-    status_12="${RED}Offline${NC}"
+    status_7="${RED}Offline${NC}"
 fi
 
-if [[ $XRAY_Vmess_None_TLS == "running" ]]; then
-    status_13="${grenbo}Online${NC}"
+if [[ $Stunnel == "running" ]]; then
+    status_8="${grenbo}Online${NC}"
 else
-    status_13="${RED}Offline${NC}"
+    status_8="${RED}Offline${NC}"
 fi
 
-if [[ $XRAY_Vless_TLS == "running" ]]; then
-    status_14="${grenbo}Online${NC}"
-else
-    status_14="${RED}Offline${NC}"
-fi
-
-if [[ $XRAY_Vless_gRPC == "running" ]]; then
-    status_15="${grenbo}Online${NC}"
-else
-    status_15="${RED}Offline${NC}"
-fi
-
-if [[ $XRAY_Vless_None_TLS == "running" ]]; then
-    status_16="${grenbo}Online${NC}"
-else
-    status_16="${RED}Offline${NC}"
-fi
-
-if [[ $Trojan_gRPC == "running" ]]; then
-    status_17="${grenbo}Online${NC}"
-else
-    status_17="${RED}Offline${NC}"
-fi
-
-if [[ $Trojan_WS == "running" ]]; then
-    status_18="${grenbo}Online${NC}"
-else
-    status_18="${RED}Offline${NC}"
-fi
-
-if [[ $Shadowsocks_WS == "running" ]]; then
-    status_19="${grenbo}Online${NC}"
-else
-    status_19="${RED}Offline${NC}"
-fi
-
-if [[ $Shadowsocks_gRPC == "running" ]]; then
-    status_20="${grenbo}Online${NC}"
-else
-    status_20="${RED}Offline${NC}"
-fi
-
-if [[ $Iptables == "exited" ]]; then
-    status_21="${grenbo}Online${NC}"
-else
-    status_21="${RED}Offline${NC}"
-fi
-
-if [[ $Squid == "running" ]]; then
-    status_22="${grenbo}Online${NC}"
-else
-    status_22="${RED}Offline${NC}"
-fi
-
-if [[ $BadVPN == "running" ]]; then
-    status_23="${grenbo}Online${NC}"
-else
-    status_23="${RED}Offline${NC}"
-fi
-
-if [[ $RClocal == "exited" ]]; then
-    status_24="${grenbo}Online${NC}"
-else
-    status_24="${RED}Offline${NC}"
-fi
-
-if [[ $Autorebot == "exited" ]]; then
-    status_25="${grenbo}Online${NC}"
-else
-    status_25="${RED}Offline${NC}"
-fi
 #IZIN
 export Server_URL="raw.githubusercontent.com/wunuit/test/main"
 License_Key=$(cat /etc/${Auther}/license.key)
@@ -289,26 +249,24 @@ echo -e "\033[93mVersion$NC             $grenbo:$NC$yellow Version 3.0$NC"
 echo -e "\033[1;93m─────────────────────────────────────\033[0m"
 echo -e "\\e[42m      ⇲ Service Information ⇲        \E[0m"
 echo -e "\033[1;93m─────────────────────────────────────\033[0m"
+echo -e "${Blue}Service Stunnel5$NC            $grenbo:$NC $status_8"
 echo -e "${Blue}Service Open_SSH$NC            $grenbo:$NC $status_1"
 echo -e "${Blue}Service Dropbear$NC            $grenbo:$NC $status_2"
-echo -e "${Blue}Service Dropbear_Websocket$NC  $grenbo:$NC $status_3"
-echo -e "${Blue}Service SSH_Websocket$NC       $grenbo:$NC $status_4"
-echo -e "${Blue}Service Nginx_Webserver$NC     $grenbo:$NC $status_6"
-echo -e "${Blue}Service Xray_Vmess_TLS$NC      $grenbo:$NC $status_11"
-echo -e "${Blue}Service Xray_Vmess_gRPC$NC     $grenbo:$NC $status_12"
-echo -e "${Blue}Service Xray_Vmess_None_TLS$NC $grenbo:$NC $status_13"
-echo -e "${Blue}Service Xray_Vless_TLS$NC      $grenbo:$NC $status_14"
-echo -e "${Blue}Service Xray_Vless_gRPC$NC     $grenbo:$NC $status_15"
-echo -e "${Blue}Service Xray_Vless_None_TLS$NC $grenbo:$NC $status_16"
-echo -e "${Blue}Service Trojan_gRPC$NC         $grenbo:$NC $status_17"
-echo -e "${Blue}Service Trojan_WS$NC           $grenbo:$NC $status_18"
-echo -e "${Blue}Service Shadowsocks_WS$NC      $grenbo:$NC $status_19"
-echo -e "${Blue}Service Shadowsocks_gRPC$NC    $grenbo:$NC $status_20"
-echo -e "${Blue}Service Iptables$NC            $grenbo:$NC $status_21"
-#echo -e "${Blue}Service Squid$NC               $grenbo:$NC $status_22"
-#echo -e "${Blue}Service BadVPN$NC              $grenbo:$NC $status_23"
-echo -e "${Blue}Service RClocal$NC             $grenbo:$NC $status_24"
-echo -e "${Blue}Service Autoreboot$NC          $grenbo:$NC $status_25"
+echo -e "${Blue}Service SSH_Websocket$NC       $grenbo:$NC $status_3"
+echo -e "${Blue}Service Nginx_Webserver$NC     $grenbo:$NC $status_4"
+echo -e "${Blue}Service Xray_Vmess_TLS$NC      $grenbo:$NC $status_5"
+echo -e "${Blue}Service Xray_Vmess_gRPC$NC     $grenbo:$NC $status_5"
+echo -e "${Blue}Service Xray_Vmess_None_TLS$NC $grenbo:$NC $status_5"
+echo -e "${Blue}Service Xray_Vless_TLS$NC      $grenbo:$NC $status_5"
+echo -e "${Blue}Service Xray_Vless_gRPC$NC     $grenbo:$NC $status_5"
+echo -e "${Blue}Service Xray_Vless_None_TLS$NC $grenbo:$NC $status_5"
+echo -e "${Blue}Service Trojan_gRPC$NC         $grenbo:$NC $status_5"
+echo -e "${Blue}Service Trojan_WS$NC           $grenbo:$NC $status_5"
+echo -e "${Blue}Service Shadowsocks_WS$NC      $grenbo:$NC $status_5"
+echo -e "${Blue}Service Shadowsocks_gRPC$NC    $grenbo:$NC $status_5"
+echo -e "${Blue}Service Iptables$NC            $grenbo:$NC $status_6"
+echo -e "${Blue}Service RClocal$NC             $grenbo:$NC $status_7"
+echo -e "${Blue}Service Autoreboot$NC          $grenbo:$NC $status_7"
 echo -e "\033[1;93m─────────────────────────────────────\033[0m"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
