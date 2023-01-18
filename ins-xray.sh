0254a33d73640b4fc6608b66b9398e670a9b311b
wunuit <112681890+wunuit@users.noreply.github.com>
2023-01-18 19:07:27 +0800

    Add files via upload

--- ins-xray.sh
+++ ins-xray.sh
@@ -731,5 +731,5 @@ sleep 1
 echo -e "[ ${GREEN}ok${NC} ] Restart & Xray & Nginx"
 systemctl daemon-reload >/dev/null 2>&1
 systemctl restart xray >/dev/null 2>&1
-systemctl restart nginx >/dev/null 2>&1
+systemctl restart nginx >/dev/null 2>&1 
 clear
